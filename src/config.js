f11c0a4936c2a87aa05687a4ff8858c900778640
= <giannalee1107@gmail.com>
2023-09-05 18:48:15 +0900

    서버 ip 삭제

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-const BASE_URL = '43.201.20.90/api';
+const BASE_URL = '/api';
+// 예정 mock server : 'https://db4d417c-9e4a-46b3-bd45-9245a9d99984.mock.pstmn.io/api';
+
 // const API = {
 //   signup: `${BASE_URL}/auth/signup`,
 //   signin: `${BASE_URL}/auth/signin`,
