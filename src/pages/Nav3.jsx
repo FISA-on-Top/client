2adad06867c0de6d8227756de212886a972f9502
wanQ-Choi <wanq0303@gmail.com>
2023-09-11 12:20:28 +0900

    API 연동

--- src/pages/Nav3.jsx
+++ src/pages/Nav3.jsx
@@ -2,7 +2,7 @@ import { React, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { StyledDatePicker } from '../styled/StyledDatePicker.jsx';
 import { ContainerDiv, WrapperDiv, ContentsDiv, TitleDiv, TextDiv, SmallContainedButton } from '../styled/StyledContents';
-import { TableContainer, Table, TableHeader, TableRow, TableCell  } from '../styled/StyledTable.jsx';
+import { TableContainer, Table, TableHeader, TableRow, TableCell } from '../styled/StyledTable.jsx';
 import BASE_URL from '../config';
 
 function Nav3() {
@@ -62,7 +62,7 @@ function Nav3() {
                 return;
             }
 
-            setData(datas.data.ipoSummary);
+            setData(datas.data);
         } catch (error) {
             console.error('Error:', error);
             alert("잠시 후 다시 시도해 주세요");
@@ -93,7 +93,7 @@ function Nav3() {
             <ContainerDiv>
                 <h1>청약 결과 조회</h1>
 
-                <WrapperDiv style={{'flex-direction': 'row'}}>
+                <WrapperDiv style={{ 'flex-direction': 'row' }}>
                     <ContentsDiv>
                         <TitleDiv>계좌 설정</TitleDiv>
                         <TextDiv>
@@ -127,20 +127,27 @@ function Nav3() {
                     </thead>
 
                     <tbody>
-                        {data.length > 0 && data.map((row, index) => (
-                            <TableRow key={index}>
-                                <TableCell>
-                                    <SmallContainedButton onClick={() => handleButtonClick(row)}>취소하기</SmallContainedButton>
-                                </TableCell>
-                                <TableCell>{row.corpCls}</TableCell>
-                                <TableCell>{row.corpName}</TableCell>
-                                <TableCell>{row.corpCode}</TableCell>
-                                <TableCell>{row.sbd} ~ {(new Date(row.sbd).getDate() + 1)}</TableCell>
-                                <TableCell>{row.refund}</TableCell>
-                                <TableCell>{row.deposit}</TableCell>
+                        {data.length > 0 ? (
+                            data.map((row, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>
+                                        <SmallContainedButton onClick={() => handleButtonClick(row)}>취소하기</SmallContainedButton>
+                                    </TableCell>
+                                    <TableCell>{row.corpCls}</TableCell>
+                                    <TableCell>{row.corpName}</TableCell>
+                                    <TableCell>{row.corpCode}</TableCell>
+                                    <TableCell>{row.sbd} ~ {(new Date(row.sbd).getDate() + 1)}</TableCell>
+                                    <TableCell>{row.refund}</TableCell>
+                                    <TableCell>{row.deposit}</TableCell>
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={7}>결과가 없습니다.</TableCell>
                             </TableRow>
-                        ))}
+                        )}
                     </tbody>
+
                 </Table>
             </TableContainer>
 
